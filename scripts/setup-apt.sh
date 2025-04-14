157d54acf482c581554054cac5e77d0a87fb361f
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2025-04-14 17:00:02 +0200

    scripts: split out non-free-firmware setup from setup-apt.sh
    The current way we handle the `non-free-firmware` component is buggy as
    the Mobian part of it can only be setup in the initial `rootfs` recipe;
    enabling it afterwards will only affect the Debian repo setup. This used
    to work as expected as `non-free-firmware` was always enabled for Mobian
    even though it wasn't always needed.
    
    This creates a new `setup-apt-nonfree.sh` which handles setting up this
    component for both the Debian and Mobian repos, ensuring they stay in
    sync.

--- scripts/setup-apt.sh
+++ scripts/setup-apt.sh
@@ -2,22 +2,14 @@
 
 DEBIAN_SUITE=$1
 SUITE=$2
-NONFREE=$3
-
-COMPONENTS="main"
-if [ "${NONFREE}" = "true" ]; then
-    COMPONENTS="${COMPONENTS} non-free-firmware"
-fi
 
 # Add debian-security for stable releases; note that only the main component is supported
 if [ "${DEBIAN_SUITE}" = "bullseye" ] || [ "${DEBIAN_SUITE}" = "bookworm" ] || [ "${DEBIAN_SUITE}" = "trixie" ]; then
     echo "deb http://security.debian.org/ ${DEBIAN_SUITE}-security main" >> /etc/apt/sources.list
 fi
 
-# Set the proper suite and components in our sources file
-sed -i -e "s/Suites: .*/Suites: ${SUITE}/" \
-       -e "s/Components: .*/Components: ${COMPONENTS}/" \
-          /etc/apt/sources.list.d/mobian.sources
+# Set the proper suite in our sources file
+sed -i "s/Suites: .*/Suites: ${SUITE}/" /etc/apt/sources.list.d/mobian.sources
 
 # Setup repo priorities so mobian comes first
 cat > /etc/apt/preferences.d/00-mobian-priority << EOF
